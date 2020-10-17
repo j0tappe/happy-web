7c43a58bb9dccb031b114d1fc49ac984093f3a79
j0tappe <jotappe-@hotmail.com>
2020-10-17 17:07:21 -0300

    Projeto Inicial

---
+++ src/database/testOld.js
@@ -0,0 +1,36 @@
+const Database = require('./db'); 
+
+Database.then(async db => {
+    // inserir dados na tabela
+    await db.run(`
+        INSERT INTO orphanages (
+            lat,
+            lng,
+            name,
+            about,
+            whatsapp,
+            images,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        ) VALUES (
+            "-27.222633",
+            "-49.6555874",
+            "Lar dos meninos",
+            "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vulnerabilidade social.",
+            "82998980101",
+            "https://images.unsplash.com/photo-1598136490941-30d885318abd?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9",
+            "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+            "Horário de visitas Das 18h até 8h",
+            "0"
+        );
+    `)
+
+    // consultar dados da tabela
+    const selectedOrphanages = await db.all("SELECT * FROM orphanages")
+    console.log(selectedOrphanages)    
+    
+    // consultar somente um orphanato, pelo ix
+    const orphanage = await db.all('SELECT * FROM orphanages WHERE id = "1"')
+    console.log(orphanage)
+})
